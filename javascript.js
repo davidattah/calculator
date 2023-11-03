a27c9882e17bbee97d78494a92de2baf383a2268
David Attah-Ogbole <d.attahogbole@gmail.com>
2023-11-03 17:57:13 -0400

    "Created divs for the basic calculator" Created container divs for the calculator and the display for the answers.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
+const body = document.querySelector("body")
+const div = document.createElement("div")
+div.classList.add("container")
+body.appendChild(div)
+
+const calculatorDiv = document.createElement("div")
+calculatorDiv.classList.add("calculator-div")
+div.appendChild(calculatorDiv)
+
+const displayDiv = document.createElement("div")
+displayDiv.classList.add("display-div")
+calculatorDiv.appendChild(displayDiv)
+
+const display = document.createElement("div")
+display.classList.add("display")
+displayDiv.appendChild(display)
+
+
+
+
+
 function add(num1,num2){
     console.log(num1 + num2)
 }
@@ -13,8 +34,6 @@ function multiply(num1,num2){
 function divide(num1,num2){
     console.log(num1 /num2)
 }
-
-
 
 function operate(num1,sign,num2){
 let firstNum = num1;
@@ -35,3 +54,4 @@ let secondNum = num2;
 }
 
 operate(2,"+",3)
+
